0c3be4b56c344acf9dd8ec931afbdb63b170933c
Otto Jung <otto.jung@vauplace.com>
2023-07-13 22:30:34 -0700

    minify all licenses

--- src/sherry/is-r7rsdecl-import-huh.scm
+++ src/sherry/is-r7rsdecl-import-huh.scm
@@ -1,16 +1,5 @@
 ;;;; Copyright (C) 2023  Otto Jung
-;;;;
-;;;; This program is free software: you can redistribute it and/or modify
-;;;; it under the terms of the GNU General Public License as published by
-;;;; the Free Software Foundation; version 3 of the License.
-;;;;
-;;;; This program is distributed in the hope that it will be useful,
-;;;; but WITHOUT ANY WARRANTY; without even the implied warranty of
-;;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-;;;; GNU General Public License for more details.
-;;;;
-;;;; You should have received a copy of the GNU General Public License
-;;;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (sherry is-r7rsdecl-import-huh)
   :export (is-r7rsdecl-import?)
