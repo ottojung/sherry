222c29ef406aeeb5536ec6df132957518e6bace6
Otto Jung <otto.jung@vauplace.com>
2023-07-14 22:05:37 -0700

    actually export the license-filepath

--- src/sherry/license.scm
+++ src/sherry/license.scm
@@ -2,7 +2,7 @@
 ;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (define-module (sherry license)
-  :export (make-license license-author license-years license-text license-prefix display-license parse-license-from-lines)
+  :export (make-license license-author license-years license-text license-prefix license-filepath display-license parse-license-from-lines)
   :use-module ((euphrates define-type9) :select (define-type9))
   :use-module ((euphrates identity) :select (identity))
   :use-module ((euphrates irregex) :select (irregex-match-substring irregex-replace irregex-search sre->irregex))
