0bdc90f3e42a698b3409c49feff0ffde0a2b7217
Otto Jung <otto.jung@vauplace.com>
2024-04-13 22:43:57 -0700

    bump version

--- src/sherry/main.scm
+++ src/sherry/main.scm
@@ -1,4 +1,4 @@
-;;;; Copyright (C) 2023  Otto Jung
+;;;; Copyright (C) 2023, 2024  Otto Jung
 ;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (current-program-path/p "sherry")
@@ -46,7 +46,7 @@
    (exit 0))
 
  (when --version
-   (display "0.1") (newline)
+   (display "0.1.1") (newline)
    (exit 0))
 
  (when <filepath>
