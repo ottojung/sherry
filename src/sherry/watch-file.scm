842609d70a946b1120719bceabd9e583edeecfaf
Otto Jung <otto.jung@vauplace.com>
2024-09-21 13:20:44 -0700

    implement basic watch procedure

--- src/sherry/watch-file.scm
+++ src/sherry/watch-file.scm
@@ -1,7 +1,40 @@
 ;;;; Copyright (C) 2024  Otto Jung
 ;;;; This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as published by the Free Software Foundation; version 3 of the License. This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details. You should have received a copy of the GNU General Public License along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-(define (watch-file <filepath>)
+(define (maybe-compile <filepath>)
+  (cond-expand
+   (guile (compile-file <filepath>))))
+
+(define (load-procedure <filepath>)
   (fix-imports/generic #f <filepath>)
-  (display "DONE!")
-  (newline))
+  (maybe-compile <filepath>)
+  (load <filepath>))
+
+(define (load-safely <filepath>)
+  (catch-any
+   (lambda whatever
+     (load-procedure <filepath>))
+   (lambda args
+     (dprintln "Errored: ~s" args))))
+
+(define (watch-file:loop <filepath>)
+  (define old-time 0)
+  (let loop ()
+    (define time (file-mtime <filepath>))
+    (unless (equal? time old-time)
+      (newline (current-error-port))
+      (display "------------------------------------------" (current-error-port))
+      (newline (current-error-port))
+      (display "Reloading..." (current-error-port))
+      (newline (current-error-port))
+      (load-safely <filepath>)
+      (set! old-time (file-mtime <filepath>))
+      (display "Done." (current-error-port))
+      (newline (current-error-port))
+      (display "------------------------------------------" (current-error-port))
+      (newline (current-error-port)))
+    (sys-usleep 20000)
+    (loop)))
+
+(define (watch-file <filepath>)
+  (watch-file:loop <filepath>))
--- src/sherry/watch-file.sld
+++ src/sherry/watch-file.sld
@@ -2,14 +2,31 @@
 (define-library
   (sherry watch-file)
   (export watch-file)
+  (import (only (euphrates catch-any) catch-any))
+  (import (only (euphrates dprintln) dprintln))
+  (import (only (euphrates file-mtime) file-mtime))
+  (import (only (euphrates sys-usleep) sys-usleep))
   (import
     (only (sherry fix-imports-generic)
           fix-imports/generic))
   (import
-    (only (scheme base) begin define newline))
+    (only (scheme base)
+          begin
+          cond-expand
+          current-error-port
+          define
+          equal?
+          lambda
+          let
+          newline
+          set!
+          unless))
+  (import (only (scheme load) load))
   (import (only (scheme write) display))
   (cond-expand
     (guile (import (only (guile) include-from-path))
+           (import
+             (only (system base compile) compile-file))
            (begin
              (include-from-path "sherry/watch-file.scm")))
     (else (include "watch-file.scm"))))
